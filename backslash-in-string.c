f28e7483a57a276b398dcc34c19646fe4e835c14
Durrrr <ihaku4@gmail.com>
2014-10-11 17:28:09 +0800

    type cast; practice 2.23 2.24 2.25

---
+++ backslash-in-string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  char *s = "xxxxxxx\
+\
+\
+\
+\
+yyyyyyyyyyyyy\
+zzzzzzzz";
+  char *s2 = "xxxx\
+  fslfl\
+  xzzz";
+
+  puts(s);
+  puts(s2);
+
+  return 0;
+}
